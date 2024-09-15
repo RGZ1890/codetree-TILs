65ebc2bd8b17100eba44ae41cdb07091cc8864b4
ArkBird <107014709+RGZ1890@users.noreply.github.com>
2024-09-15 22:01:19 +0900

    Title: 행복한 수열의 개수, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240915/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240915/행복한 수열의 개수/number-of-happy-sequence.py
@@ -12,7 +12,8 @@ def HappyList(row, n, m):
     return 0
 
 answer = 0
-if (n == 1) answer = 1
+if (n == 1):
+    answer = 1
 for i in range(n):
     board[i] = list(map(int, sys.stdin.readline().split()))
     answer += HappyList(board[i], n, m)
