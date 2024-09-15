2baff828b8826beca8b5122224ef6390cbfa4409
ArkBird <107014709+RGZ1890@users.noreply.github.com>
2024-09-15 22:48:51 +0900

    Title: 트로미노, Time: 149ms, Memory: 26MB, Status: Passed - CodeTree

--- 240915/트로미노/tromino.py
+++ 240915/트로미노/tromino.py
@@ -1,31 +1,40 @@
 import sys
 
 n, m = map(int, sys.stdin.readline().split())
-board = [[0] * n for _ in range(m)]
+board = [[0] * m for _ in range(n)]
 answer = 0
 
 for i in range(n):
     board[i] = list(map(int, sys.stdin.readline().split()))
 
-def Lshaped(board, res):
+def Lshaped(board, res, n, m):
     for i in range(1, n):
-        for j in range(1, n):
+        for j in range(1, m):
             subList = [board[i - 1][j - 1], board[i - 1][j],\
                         board[i][j - 1], board[i][j]]
+#           print(subList)
             s = sum(subList) - min(subList)
             res = max(s, res)
     return res
 
-def Ishaped(board, res):
-    for row in board:
-        for j in range(n - 3):
-            res = max(sum(row[j:j + 3]), res)
-    for i in range(n):
-        col = [board[i][j] for j in range(n)]
-        for j in range(n - 3):
-            res = max(sum(row[j:j + 3]), res)
+def vertical(board, res, n, m):
+    for j in range(m):
+        col = [board[i][j] for i in range(n)]
+        for i in range(2, n):
+#           print("col", col[i - 2:i + 1])
+            res = max(sum(col[i - 2:i + 1]), res)
     return res
 
-answer = Lshaped(board, answer)
-answer = Ishaped(board, answer)
+def horizontal(board, res, n, m):
+    for i in range(n):
+        row = board[i]
+        for j in range(2, m):
+#           print("row", row[j - 2:j + 1])
+            res = max(sum(row[j - 2:j + 1]), res)
+    return res
+
+answer = Lshaped(board, answer, n, m)
+answer = vertical(board, answer, n, m)
+answer = horizontal(board, answer, n, m)
+
 print(answer)
