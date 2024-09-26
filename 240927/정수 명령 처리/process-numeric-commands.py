f1e107e87b029d9a0745ebcaef12a7ecc97b3b01
Sukhyun Nam <107014709+RGZ1890@users.noreply.github.com>
2024-09-27 00:52:08 +0900

    Title: 정수 명령 처리, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240927/정수 명령 처리/process-numeric-commands.py
@@ -0,0 +1,54 @@
+import sys
+
+max_size = 100
+
+class Stack:
+	
+	def __init__(self):
+		self.items = [0] * max_size
+		self.size = 0
+		self.top = None
+	
+	def empty(self):
+		return 0 if self.size else 1
+	
+	def top(self):
+		if self.empty():
+			raise Exception("Stack is Empty")
+		return self.top
+	
+	def push(self, num):
+		self.size += 1
+		self.items[self.size - 1] = num
+		self.top = num
+		
+	def pop(self):
+		if self.empty():
+			raise Exception("Stack is empty")
+		self.size -= 1
+		self.top = None if self.empty() else self.items[self.size - 1]
+		return self.items[self.size]
+	
+def main():
+	stack = Stack()
+
+	N = int(sys.stdin.readline())
+	for i in range(N):
+		args = sys.stdin.readline().split()
+		cmd = args[0]
+		if cmd == "push":
+			stack.push(args[1])
+		elif cmd == "pop":
+			print(stack.pop())
+		elif cmd == "size":
+			print(stack.size)
+		elif cmd == "empty":
+			print(stack.empty())
+		elif cmd == "top":
+			print(stack.top())
+		else:
+			raise Exception("Wrong argument.")
+
+
+if __name__ == "__main__":
+	main()
