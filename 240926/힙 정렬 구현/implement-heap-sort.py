172269e53b35f3ca0c657c15ead2a327cb1153b5
Sukhyun Nam <107014709+RGZ1890@users.noreply.github.com>
2024-09-26 22:00:17 +0900

    Title: 힙 정렬 구현, Time: 876ms, Memory: 34MB, Status: Passed - CodeTree

---
+++ 240926/힙 정렬 구현/implement-heap-sort.py
@@ -0,0 +1,34 @@
+import sys
+
+def heapify(arr_heap, n, idx):
+	lc, rc = idx * 2, idx * 2 + 1
+	largest = idx
+	
+	if n >= lc:
+		largest = lc if arr_heap[largest] < arr_heap[lc] else largest
+		if n >= rc:
+			largest = rc if arr_heap[largest] < arr_heap[rc] else largest
+	if largest != idx:
+		arr_heap[largest], arr_heap[idx] = arr_heap[idx], arr_heap[largest]
+		heapify(arr_heap, n, largest)
+		
+def heap_sort(arr, n):
+	arr_heap = arr
+	for i in range(n // 2, 0, -1):
+		heapify(arr_heap, n, i)
+	for i in range(n, 1, -1):
+		arr_heap[1], arr_heap[i] = arr_heap[i], arr_heap[1]
+		heapify(arr_heap, i - 1, 1)
+	
+	return arr_heap[1:]
+
+def main():
+	n = int(sys.stdin.readline())
+	arr = [0] + list(map(int, sys.stdin.readline().split()))
+	
+	arr_sorted = heap_sort(arr, n)
+	
+	print(*arr_sorted)
+
+if __name__ == "__main__":
+	main()
