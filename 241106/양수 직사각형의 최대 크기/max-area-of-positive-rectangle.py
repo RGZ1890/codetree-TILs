a419d059f2bfb02ce43c05f5c2fc638c8ef5cd58
Sukhyun Nam <107014709+RGZ1890@users.noreply.github.com>
2024-11-06 14:55:41 +0900

    Title: 양수 직사각형의 최대 크기, Time: 80ms, Memory: 18MB, Status: Passed - CodeTree

--- 241106/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
+++ 241106/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
@@ -1,7 +1,7 @@
 def checkArea(board, sr, sc, er, ec):
 	for r in range(sr, er):
 		for c in range(sc, ec):
-			if board[r][c] < 0:
+			if board[r][c] <= 0:
 				return -1
 	return (er - sr) * (ec - sc)
 
@@ -10,11 +10,11 @@ def getArea(board, n, m):
 	size = -1
 	for sr in range(n):
 		for sc in range(m):
-			if board[sr][sc] < 0:
+			if board[sr][sc] <= 0:
 				continue
 			for er in range(sr + 1, n + 1):
 				for ec in range(sc + 1, m + 1):
-					if board[er - 1][ec - 1] < 0:
+					if board[er - 1][ec - 1] <= 0:
 						continue
 					size = max(checkArea(board, sr, sc, er, ec), size)
 	return size
