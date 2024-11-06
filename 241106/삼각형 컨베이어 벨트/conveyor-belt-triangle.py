92a443c88d5e0e5c26fa3e3800ec3eb1eb337c57
Sukhyun Nam <107014709+RGZ1890@users.noreply.github.com>
2024-11-06 15:45:40 +0900

    Title: 삼각형 컨베이어 벨트, Time: 83ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 241106/삼각형 컨베이어 벨트/conveyor-belt-triangle.py
+++ 241106/삼각형 컨베이어 벨트/conveyor-belt-triangle.py
@@ -1,29 +1,18 @@
-def rotBoard(board, n):
-	left = board[2][n - 1]
-	middle = board[0][n - 1]
-	right = board[1][n - 1]
-	
-	for i in range(3):
-		for j in range(n - 2, -1, -1):
-			board[i][j + 1] = board[i][j]
-	board[0][0] = left
-	board[1][0] = middle
-	board[2][0] = right
-	
-	return board
-
-
 def main():
 	n, t = map(int, input().split())
 	board = [[0] * n for _ in range(3)]
 	for i in range(3):
 		board[i] = list(map(int, input().split()))
-		
-	for i in range(t):
-		board = rotBoard(board, n)
 	
-	for row in board:
-		print(*row)
+	allR = board[0] + board[1] + board[2]
+	l = 3 * n
+	for i in range(t):
+		allR = [allR[l - 1]] + allR[:l - 1]
+	
+	for i in range(3):
+		for j in range(n):
+			print(allR[3 * i + j], end = ' ')
+		print()
 
 
 if __name__ == "__main__":
