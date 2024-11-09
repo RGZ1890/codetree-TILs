18af2e51ef8e230d6ba636f05a3471f6b23c38a3
Sukhyun Nam <107014709+RGZ1890@users.noreply.github.com>
2024-11-10 01:08:13 +0900

    Title: 1차원 젠가, Time: 44ms, Memory: 15MB, Status: Passed - CodeTree

--- 241110/1차원 젠가/jenga-1d.py
+++ 241110/1차원 젠가/jenga-1d.py
@@ -1,3 +1,5 @@
+import sys
+
 def moveBlock(block, s, e):
 	l = len(block)
 	tmp = [0] * (l - (e - s + 1))
@@ -11,13 +13,13 @@ def moveBlock(block, s, e):
 
 
 def main():
-	n = int(input())
+	n = int(sys.stdin.readline())
 	block = [0] * n
 	for i in range(n):
-		block[i] = int(input())
+		block[i] = int(sys.stdin.readline())
 	
-	s1, e1 = map(int, input().split())
-	s2, e2 = map(int, input().split())
+	s1, e1 = map(int, sys.stdin.readline().split())
+	s2, e2 = map(int, sys.stdin.readline().split())
 	
 	block = moveBlock(block, s1, e1)
 	block = moveBlock(block, s2, e2)
