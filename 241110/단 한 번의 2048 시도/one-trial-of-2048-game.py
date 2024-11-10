79890a982dcc66429dc740de294deb28228f2de3
Sukhyun Nam <107014709+RGZ1890@users.noreply.github.com>
2024-11-10 23:00:35 +0900

    Title: 단 한 번의 2048 시도, Time: 86ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 241110/단 한 번의 2048 시도/one-trial-of-2048-game.py
+++ 241110/단 한 번의 2048 시도/one-trial-of-2048-game.py
@@ -1,9 +1,8 @@
-directions = {'L': -1, 'R': 1, 'U': -2, 'D': 2}
-
 def zfil(board):
 	for i in range(4):
-		l = len(board[i])
-		board[i] = board[i] + [0] * (4 - l)
+		for j in range(1, 4):
+			if board[i][j - 1] == 0:
+				board[i][j], board[i][j - 1] = board[i][j - 1], board[i][j]
 	
 	return board
 
@@ -16,33 +15,27 @@ def comb(board):
 				break
 			if board[i][j] == board[i][j - 1]:
 				board[i][j - 1], board[i][j] = 2 * board[i][j - 1], 0
-				
+	board = zfil(board)
+	
 	return board
 
 
 def move(board, d):
-	if d == -1:
-		tmpBoard = [[cell for cell in row if cell != 0] for row in board]
-	elif d == 1:
-		tmpBoard = [[cell for cell in row[::-1] if cell != 0] for row in board]
-	elif d == -2:
-		tmpBoard = [[board[i][j] for i in range(4) if board[i][j] != 0] for j in range(4)]
-	else: # D
-		tmpBoard = [[board[i][j] for i in range(3, -1, -1) if board[i][j] != 0] for j in range(4)]
+	if d == 'R':
+		tmpBoard = [[cell for cell in row[::-1]] for row in board]
+	elif d == 'U':
+		tmpBoard = [[board[i][j] for i in range(4)] for j in range(4)]
+	elif d == 'D':
+		tmpBoard = [[board[i][j] for i in range(3, -1, -1)] for j in range(4)]
 		
 	tmpBoard = zfil(tmpBoard)
 	tmpBoard = comb(tmpBoard)
 	
-	tmpBoard = [[cell for cell in row if cell != 0] for row in tmpBoard]
-	tmpBoard = zfil(tmpBoard)
-	
-	if d == -1:
-		tmpBoard = [[cell for cell in row] for row in tmpBoard]
-	elif d == 1:
+	if d == 'R':
 		tmpBoard = [[cell for cell in row[::-1]] for row in tmpBoard]
-	elif d == -2:
+	elif d == 'U':
 		tmpBoard = [[tmpBoard[i][j] for i in range(4)] for j in range(4)]
-	else:
+	elif d == 'D':
 		tmpBoard = [[tmpBoard[i][j] for i in range(4)] for j in range(3, -1, -1)]
 	
 	return tmpBoard
@@ -53,7 +46,6 @@ def main():
 	for i in range(4):
 		board[i] = list(map(int, input().split()))
 	d = input()
-	d = directions[d]
 	board = move(board, d)
 	for row in board:
 		print(*row)
