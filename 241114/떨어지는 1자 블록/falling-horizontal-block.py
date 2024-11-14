f1b02fe9394fc35cc4d870970423a83a08e31ee5
Sukhyun Nam <107014709+RGZ1890@users.noreply.github.com>
2024-11-14 18:27:19 +0900

    Title: 떨어지는 1자 블록, Time: 52ms, Memory: 16MB, Status: Passed - CodeTree

--- 241114/떨어지는 1자 블록/falling-horizontal-block.py
+++ 241114/떨어지는 1자 블록/falling-horizontal-block.py
@@ -1,6 +1,6 @@
 def moveable(board, n, block, r):
 	if r >= n:
-		return True
+		return False
 	for j in range(block[0], block[1]):
 		if board[r][j] != 0:
 			return False
@@ -15,10 +15,10 @@ def main():
 	
 	block = [k - 1, k + m - 1]
 	
-	for row in range(n):
-		if not moveable(board, n, block, row + 1):
+	for r in range(n):
+		if not moveable(board, n, block, r + 1):
 			for j in range(block[0], block[1]):
-				board[row][j] = 1
+				board[r][j] = 1
 			break
 	
 	for row in board:
