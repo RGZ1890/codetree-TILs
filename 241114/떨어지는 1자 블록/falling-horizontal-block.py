ef7e7f1e1111668a97e854a42247507c5983fe4c
Sukhyun Nam <107014709+RGZ1890@users.noreply.github.com>
2024-11-14 18:08:55 +0900

    Title: 떨어지는 1자 블록, Time: 77ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 241114/떨어지는 1자 블록/falling-horizontal-block.py
@@ -0,0 +1,27 @@
+def moveable(board, n, block, r):
+	for j in range(block[0], block[1]):
+		if board[r][j] != 0:
+			return False
+	return True
+
+
+def main():
+	n, m, k = map(int, input().split())
+	board = [[0] * n for _ in range(n)]
+	for i in range(n):
+		board[i] = list(map(int, input().split()))
+	
+	block = [k - 1, k + m - 1]
+	
+	for row in range(n):
+		if not moveable(board, n, block, row + 1):
+			for j in range(block[0], block[1]):
+				board[row][j] = 1
+			break
+	
+	for row in board:
+		print(*row)
+
+
+if __name__ == "__main__":
+	main()
