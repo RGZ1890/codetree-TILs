9bd12d2a702d309ced722614842b49ed3452e2f9
Sukhyun Nam <107014709+RGZ1890@users.noreply.github.com>
2024-11-16 20:59:00 +0900

    Title: 벽 짚고 미로 탈출하기, Time: 51ms, Memory: 17MB, Status: Wrong Answer - CodeTree

--- 241116/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.py
+++ 241116/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.py
@@ -1,63 +1,50 @@
 dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]]
 
-def turn(d, bound):
-	if bound == 'L':
-		return 3 if d == 0 else d - 1
-	return 0 if d == 3 else d + 1
 
-
-def move(board, N, cpos, cdir):
+def move(board, cpos, cdir):
 	npos = [cpos[0] + dirs[cdir][0], cpos[1] + dirs[cdir][1]]
-#	print(cpos, cdir)
 	if board[npos[0]][npos[1]] == 1:
-		for i in range(4):
-			cdir = turn(cdir, 'L')
+		for i in range(3):
+			cdir = cdir - 1 if cdir > 0 else 3
 			npos = [cpos[0] + dirs[cdir][0], cpos[1] + dirs[cdir][1]]
 			if board[npos[0]][npos[1]] == 1:
 				npos = cpos
 			else:
 				break
-			
-	elif board[npos[0]][npos[1]] == 0:
-		ndir = turn(cdir, 'R')
+	if board[npos[0]][npos[1]] == 0:
+		ndir = cdir + 1 if cdir < 3 else 0
 		nnpos = [npos[0] + dirs[ndir][0], npos[1] + dirs[ndir][1]]
-		if board[nnpos[0]][nnpos[1]] == 1:
-			return npos, cdir
-		return npos, ndir
+		if board[nnpos[0]][nnpos[1]] == 0:
+			return npos, ndir
+		
 	return npos, cdir
 
-
-def trav(board, N, cpos, start):
-	t = 0
-	cdir = 1
-	
-	while True:
-		t += 1
-		npos, ndir = move(board, N, cpos, cdir)
-		if board[npos[0]][npos[1]] == -1:
-			break
-		if npos == start or npos == cpos:
-			return -1
-		cpos, cdir = npos, ndir
-	
-	return t
 
 
 def main():
 	N = int(input())
 	board = [[-1] * (N + 2) for _ in range(N + 2)]
-	x, y = map(int, input().split())
-	start = cur = [x, y]
-
+	cpos = start = list(map(int, input().split()))
 	for i in range(1, N + 1):
 		s = input()
-		for j in range(N):
-			board[i][j + 1] = 1 if s[j] == '#' else 0
+		for j in range(1, N + 1):
+			board[i][j] = 1 if s[j - 1] == '#' else 0
+			
+	answer = 0
+	cdir = 1
 	
-	answer = trav(board, N, cur, start)
-	
+	while True:
+		answer += 1
+		npos, ndir = move(board, cpos, cdir)
+#		print(cpos, '->', npos, cdir, '->', ndir)
+		if npos == start or npos == cpos:
+			answer = -1
+			break
+		elif board[npos[0]][npos[1]] == -1:
+			break
+		cpos, cdir = npos, ndir
+
 	print(answer)
-		
 
 
 if __name__ == "__main__":
