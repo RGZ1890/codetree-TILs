46803c1221179b57f22b101b94286178a6a92504
Sukhyun Nam <107014709+RGZ1890@users.noreply.github.com>
2024-11-16 22:48:03 +0900

    Title: 주사위 던지기, Time: 79ms, Memory: 18MB, Status: Passed - CodeTree

--- 241116/주사위 던지기/roll-a-dice.py
+++ 241116/주사위 던지기/roll-a-dice.py
@@ -3,15 +3,12 @@ top, front, right = 0, 1, 2
 
 def rolldice(dice, comm):
 	if comm == 'L':
-		dice[top], dice[front], dice[right] = dice[right], dice[front], dice[top][::-1]
+		return [dice[right], dice[front], dice[top][::-1]]
 	elif comm == 'R':
-		dice[top], dice[front], dice[right] = dice[right][::-1], dice[front], dice[top]
+		return [dice[right][::-1], dice[front], dice[top]]
 	elif comm == 'U':
-		dice[top], dice[front], dice[right] = dice[front], dice[top][::-1], dice[right]
-	else:
-		dice[top], dice[front], dice[right] = dice[front][::-1], dice[top], dice[right]
-		
-	return dice
+		return [dice[front], dice[top][::-1], dice[right]]
+	return [dice[front][::-1], dice[top], dice[right]]
 
 
 def main():
@@ -19,23 +16,17 @@ def main():
 	board = [[0] * n for _ in range(n)]
 	cur = [r - 1, c - 1]
 	dice = [[1, 6], [2, 5], [3, 4]]
-	comms = input().split()
 	board[cur[0]][cur[1]] = dice[0][1]
 	
+	comms = input().split()
 	for c in comms:
 		nex = [cur[0] + dirs[c][0], cur[1] + dirs[c][1]]
 		if 0 <= nex[0] < n and 0 <= nex[1] < n:
 			cur = nex
 			dice = rolldice(dice, c)
 			board[cur[0]][cur[1]] = dice[0][1]
-		
-#		print("===============")
-#		for row in board:
-#			print(*row)
-	
-	answer = 0
-	for row in board:
-		answer += sum(row)
+			
+	answer = sum([sum(row) for row in board])
 	print(answer)
 
 
