1419875e532505fc4d15ca03a80510c7acc0c7c6
Sukhyun Nam <107014709+RGZ1890@users.noreply.github.com>
2024-12-25 22:57:52 +0900

    Title: 안전 지대, Time: 76ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 241225/안전 지대/comfort-zone.py
+++ 241225/안전 지대/comfort-zone.py
@@ -1,12 +1,13 @@
 dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]]
 
-def dfs(board, visited, K, cur):
+def dfs(board, N, M, visited, K, cur):
     for d in dirs:
         next_pos = [cur[0] + d[0], cur[1] + d[1]]
-        if board[next_pos[0]][next_pos[1]] > K and \
-        not visited[next_pos[0]][next_pos[1]]:
+        if 0 <= next_pos[0] < N and 0 <= next_pos[1] < M \
+        and board[next_pos[0]][next_pos[1]] > K \
+        and not visited[next_pos[0]][next_pos[1]]:
             visited[next_pos[0]][next_pos[1]] = True
-            visited = dfs(board, visited, K, next_pos)
+            visited = dfs(board, N, M, visited, K, next_pos)
     
     return visited
 
@@ -14,17 +15,15 @@ def dfs(board, visited, K, cur):
 def solution(board, N, M, thres):
     ans_k, ans_cnt = -1, -1
     for K in range(1, thres + 1):
-        visited = [[True] + [False] * M + [True] for _ in range(N + 2)]
-        visited[0], visited[N + 1] = [[False] * (M + 2)], [[False] * (M + 2)]
-        
+        visited = [[False] * M for _ in range(N)]
         cnt = 0
         
-        for i in range(1, N + 1):
-            for j in range(1, M + 1):
+        for i in range(N):
+            for j in range(M):
                 if board[i][j] > K and not visited[i][j]:
                     visited[i][j] = True
                     cnt += 1
-                    visited = dfs(board, visited, K, [i, j])
+                    visited = dfs(board, N, M, visited, K, [i, j])
         
         if cnt > ans_cnt:
             ans_k, ans_cnt = K, cnt
@@ -34,13 +33,12 @@ def solution(board, N, M, thres):
 
 def main():
     N, M = map(int, input().split())
-    board = [[0] * (M + 2) for _ in range(N + 2)]
+    board = [[0] * M for _ in range(N)]
     thres = 0
     
-    for i in range(1, N + 1):
-        tmp = list(map(int, input().split()))
+    for i in range(N):
+        board[i] = list(map(int, input().split()))
         thres = max(thres, max(tmp))
-        board[i] = [0] + tmp + [0]
         
     solution(board, N, M, thres)
     
