df7ce99154105464ad6f3e5a81cd7f7a5ec52988
Sukhyun Nam <107014709+RGZ1890@users.noreply.github.com>
2024-12-25 22:49:39 +0900

    Title: 안전 지대, Time: 48ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241225/안전 지대/comfort-zone.py
@@ -0,0 +1,47 @@
+dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+
+def dfs(board, visited, K, cur):
+    for d in dirs:
+        next_pos = [cur[0] + d[0], cur[1] + d[1]]
+        if board[next_pos[0]][next_pos[1]] > K and \
+        not visited[next_pos[0]][next_pos[1]]:
+            visited[next_pos[0]][next_pos[1]] = True
+            visited = dfs(board, visited, K, next_pos)
+    
+    return visited
+
+
+def solution(board, N, M, thres):
+    ans_k, ans_cnt = 0, 0
+    for K in range(1, thres):
+        visited = [[False] * (M + 2) for _ in range(N + 2)]
+        cnt = 0
+        
+        for i in range(1, N + 1):
+            for j in range(1, M + 1):
+                if board[i][j] > K and not visited[i][j]:
+                    visited[i][j] = True
+                    cnt += 1
+                    visited = dfs(board, visited, K, [i, j])
+    
+        if cnt > ans_cnt:
+            ans_k, ans_cnt = K, cnt
+        
+    print(ans_k, ans_cnt)
+
+
+def main():
+    N, M = map(int, input().split())
+    board = [[0] * (M + 2) for _ in range(N + 2)]
+    thres = 0
+    
+    for i in range(1, N + 1):
+        tmp = list(map(int, input().split()))
+        thres = max(thres, max(tmp))
+        board[i] = [0] + tmp + [0]
+        
+    solution(board, N, M, thres)
+    
+
+if __name__ == "__main__":
+    main()
