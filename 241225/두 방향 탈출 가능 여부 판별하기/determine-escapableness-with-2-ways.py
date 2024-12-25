d6f108ae9aa94c9012b0a2eacfaab33e269b77e7
Sukhyun Nam <107014709+RGZ1890@users.noreply.github.com>
2024-12-25 21:53:24 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 51ms, Memory: 17MB, Status: Passed - CodeTree

--- 241225/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 241225/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -1,19 +1,18 @@
 dirs = [[1, 0], [0, 1]]
 
-def solution(board, end, cur, visited, ans):
+def solution(board, end, cur, visited, res):
     if cur == end:
-        return True
+        return 1
     
     for d in dirs:
         new_pos = [cur[0] + d[0], cur[1] + d[1]]
         if board[new_pos[0]][new_pos[1]] == 1 and \
         not visited[new_pos[0]][new_pos[1]]:
             visited[new_pos[0]][new_pos[1]] = True
-            ans = solution(board, end, new_pos, visited, ans)
-            if ans:
-                return ans
+            if solution(board, end, new_pos, visited, res):
+                return 1
             
-    return ans
+    return 0
 
 
 def main():
@@ -25,8 +24,7 @@ def main():
         inp = list(map(int, input().split()))
         board[i] = [0] + inp + [0]
         
-    ans = solution(board, [n, m], [1, 1], visited, False)
-    print(1 if ans else 0)
+    print(solution(board, [n, m], [1, 1], visited, 0))
         
         
 if __name__ == "__main__":
