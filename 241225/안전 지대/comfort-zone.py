28e877717853c4dbbd4cc45d974251ae7b8fbdb6
Sukhyun Nam <107014709+RGZ1890@users.noreply.github.com>
2024-12-25 23:12:44 +0900

    Title: 안전 지대, Time: 632ms, Memory: 120MB, Status: Passed - CodeTree

--- 241225/안전 지대/comfort-zone.py
+++ 241225/안전 지대/comfort-zone.py
@@ -1,9 +1,12 @@
+import sys
+sys.setrecursionlimit(2501)
+
 dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]]
 
 def dfs(tmp_board, N, M, visited, K, cur):
     for d in dirs:
         next_pos = [cur[0] + d[0], cur[1] + d[1]]
-        if tmp_board[next_pos[0]][next_pos[1]]\
+        if tmp_board[next_pos[0]][next_pos[1]] > K\
         and not visited[next_pos[0]][next_pos[1]]:
             visited[next_pos[0]][next_pos[1]] = True
             visited = dfs(tmp_board, N, M, visited, K, next_pos)
@@ -14,18 +17,17 @@ def dfs(tmp_board, N, M, visited, K, cur):
 def solution(board, N, M, thres):
     ans_k, ans_cnt = -1, -1
     for K in range(1, thres + 1):
-        tmp_board = [[True if cell > K else False for cell in row] for row in board]
         visited = [[True] + [False] * M + [True] for _ in range(N + 2)]
         visited[0], visited[N + 1] = [True] * (M + 2), [True] * (M + 2)
         cnt = 0
         
         for i in range(1, N + 1):
             for j in range(1, M + 1):
-                if tmp_board[i][j] and not visited[i][j]:
+                if board[i][j] > K and not visited[i][j]:
                     visited[i][j] = True
                     cnt += 1
-                    visited = dfs(tmp_board, N, M, visited, K, [i, j])
-#       print(K, cnt)
+                    visited = dfs(board, N, M, visited, K, [i, j])
+#       print("RES", K, cnt)
         if cnt > ans_cnt:
             ans_k, ans_cnt = K, cnt
         
