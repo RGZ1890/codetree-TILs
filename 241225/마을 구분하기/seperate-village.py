cc12c12b15010cd6fc4ff9d8913fc323f43a6dac
Sukhyun Nam <107014709+RGZ1890@users.noreply.github.com>
2024-12-25 22:04:35 +0900

    Title: 마을 구분하기, Time: 42ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241225/마을 구분하기/seperate-village.py
@@ -0,0 +1,41 @@
+dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+
+
+def dfs(board, visited, cur, cnt):
+    for d in dirs:
+        new_pos = [cur[0] + d[0], cur[1] + d[1]]
+        if board[new_pos[0]][new_pos[1]] != 0 and \
+        not visited[new_pos[0]][new_pos[1]]:
+            visited[new_pos[0]][new_pos[1]] = True
+            return dfs(board, visited, new_pos, cnt + 1)
+    
+    return visited, cnt
+
+
+def solution(board, n):
+    ans = []
+    visited = [[False] * (n + 2) for _ in range(n + 2)]
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            if board[i][j] != 0 and not visited[i][j]:
+                visited[i][j] = True
+                visited, cnt = dfs(board, visited, [i, j], 1)
+                ans.append(cnt)
+                
+    return sorted(ans)
+
+
+def main():
+    n = int(input())
+    
+    board = [[0] * (n + 2) for _ in range(n + 2)]
+    for i in range(1, n + 1):
+        tmp = list(map(int, input().split()))
+        board[i] = [0] + tmp + [0]
+    
+    ans = solution(board, n)
+    print(len(ans), *ans, sep = '\n')
+        
+
+if __name__ == "__main__":
+    main()
