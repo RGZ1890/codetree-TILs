9d6f862c899e18ffc0c5543bfc28f0faa0bbc03b
Sukhyun Nam <107014709+RGZ1890@users.noreply.github.com>
2024-12-25 21:26:29 +0900

    Title: 그래프 탐색, Time: 94ms, Memory: 20MB, Status: Passed - CodeTree

--- 241225/그래프 탐색/graph-traversal.py
+++ 241225/그래프 탐색/graph-traversal.py
@@ -1,22 +1,23 @@
 
-def solution(adj_mat, N, visited, cur, ans):
-    for i in range(N):
-        if adj_mat[cur][i] and not visited[i]:
-            visited[i] = True
-            ans = solution(adj_mat, N, visited, i, ans + 1)
+def solution(adj_list, N, visited, cur, ans):
+    for adj in adj_list[cur]:
+        if not visited[adj]:
+            visited[adj] = True
+            ans = solution(adj_list, N, visited, adj, ans + 1)
             
     return ans
 
 
 def main():
     N, M = map(int, input().split())
-    adj_mat = [[0] * N for _ in range(N)]
+    adj_list = [[] for _ in range(N)]
     visited = [True] + [False] * (N - 1)
     for i in range(M):
         x, y = map(int, input().split())
-        adj_mat[x - 1][y - 1], adj_mat[y - 1][x - 1] = True, True
+        adj_list[x - 1].append(y - 1)
+        adj_list[y - 1].append(x - 1)
     
-    ans = solution(adj_mat, N, visited, 0, 0)
+    ans = solution(adj_list, N, visited, 0, 0)
     print(ans)
     
     
