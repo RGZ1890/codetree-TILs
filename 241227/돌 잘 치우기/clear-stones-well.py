50f7470b9539a7fbeb700cf30a9fcc4a33a83dc7
Sukhyun Nam <107014709+RGZ1890@users.noreply.github.com>
2024-12-27 01:58:13 +0900

    Title: 돌 잘 치우기, Time: 74ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 241227/돌 잘 치우기/clear-stones-well.py
@@ -0,0 +1,67 @@
+from collections import deque
+
+dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+
+def bfs(board, n, s, avail):
+    visited = [[False] * n for _ in range(n)]
+    visited[s[0]][s[1]] = True
+    q = deque()
+    q.append(s)
+    
+    while q:
+        cur = q.popleft()
+        for d in dirs:
+            nex = [cur[0] + d[0], cur[1] + d[1]]
+            if 0 <= nex[0] < n and 0 <= nex[1] < n \
+            and board[nex[0]][nex[1]] == 0 \
+            and not visited[nex[0]][nex[1]]:
+                visited[nex[0]][nex[1]] = True
+                avail.add((nex[0], nex[1]))
+                q.append(nex)
+                
+    return avail
+
+
+def solution(board, n, starts, rocks, m):
+    ans = 0
+    for _ in range(m):
+        best_pos = [-1, -1]
+        best_cnt = 0
+        for r in rocks:
+            board[r[0]][r[1]] = 0
+            avail = set()
+            for s in starts:
+                avail = bfs(board, n, s, avail)
+                
+            cnt = len(avail)
+            if best_cnt < cnt:
+                best_cnt = cnt
+                best_pos = r
+            board[r[0]][r[1]] = 1
+            
+        board[best_pos[0]][best_pos[1]] = 0
+        
+    return best_cnt
+    
+
+def main():
+    n, k, m = map(int, input().split())
+    board = [[0] * n for _ in range(n)]
+    starts = [[0, 0] for _ in range(k)]
+    rocks = []
+    for i in range(n):
+        board[i] = list(map(int, input().split()))
+        for j in range(n):
+            if board[i][j] == 1:
+                rocks.append([i, j])
+    
+    for i in range(k):
+        r, c = map(int, input().split())
+        starts[i] = [r - 1, c - 1]
+        
+    print(solution(board, n, starts, rocks, m))
+        
+        
+if __name__ == "__main__":
+    main()
+        
