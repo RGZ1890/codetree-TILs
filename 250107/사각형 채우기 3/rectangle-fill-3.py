ff68acab5e1ad37a271baf8392d619ca6e7c542e
Sukhyun Nam <107014709+RGZ1890@users.noreply.github.com>
2025-01-07 02:42:04 +0900

    Title: 사각형 채우기 3, Time: 47ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 250107/사각형 채우기 3/rectangle-fill-3.py
@@ -0,0 +1,18 @@
+DP = [0] * 1001
+DP[1], DP[2] = 2, 7
+
+def solution(N):
+	if DP[N] != 0:
+		return DP[N]
+	DP[N] = 1 * solution(N - 1) + (N - 1) * solution(1) \
+			+ 2 * solution(N - 2) + (N - 2) * solution(2)
+	return DP[N]
+
+
+def main():
+	n = int(input())
+	print(solution(n))
+
+
+if __name__ == "__main__":
+	main()
