1dfd0e2ee800911a1a2ccb11778274f095987a04
Sukhyun Nam <107014709+RGZ1890@users.noreply.github.com>
2025-01-11 02:03:48 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250111/정수 사각형 최솟값의 최대/maximin-path-in-square.py
@@ -0,0 +1,29 @@
+NOPE = 1000001
+
+def solution(board, N):
+	ans_board = [[NOPE] * (N + 2) for _ in range(N + 2)]
+	for i in range(1, N + 1):
+		ans_board[i] = [NOPE] + board[i - 1] + [NOPE]
+	
+	for i in range(1, N + 1):
+		for j in range(1, N + 1):
+			if [i, j] == [1, 1]:
+				continue
+			ans_board[i][j] = min(ans_board[i][j],
+				max(ans_board[i - 1][j], ans_board[i][j - 1]))
+			
+	
+	return ans_board[N][N]
+
+
+def main():
+	N = int(input())
+	board = [[0] * N for _ in range(N)]
+	for i in range(N):
+		board[i] = list(map(int, input().split()))
+	
+	print(solution(board, N))
+
+
+if __name__ == "__main__":
+	main()
