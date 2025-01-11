c3d951fd78bb1af4a411f3c9f97711485b95a062
Sukhyun Nam <107014709+RGZ1890@users.noreply.github.com>
2025-01-11 22:27:05 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250111/정수 사각형 최솟값의 최대/maximin-path-in-square.py
+++ 250111/정수 사각형 최솟값의 최대/maximin-path-in-square.py
@@ -1,27 +1,24 @@
 NOPE = 1000001
 
-def getVal(ans_board, cur, cand):
-	return min(ans_board[cur[0]][cur[1]], ans_board[cand[0]][cand[1]])
-
 
 def solution(board, N):
-	ans_board = [[NOPE] * (N + 2) for _ in range(N + 2)]
+	sboard = [[NOPE] * (N + 2) for _ in range(N + 2)]
 	for i in range(1, N + 1):
-		ans_board[i] = [NOPE] + board[i - 1] + [NOPE]
+		sboard[i] = [NOPE] + board[i - 1] + [NOPE]
 	
 	for i in range(1, N + 1):
 		for j in range(1, N + 1):
 			if [i, j] == [1, 1]:
 				continue
-			cur, left, upper = [i, j], [i - 1, j], [i, j - 1]
-			ans_board[i][j] = max(getVal(ans_board, cur, upper), getVal(ans_board, cur, left))
+			sboard[i][j] = min(sboard[i][j], max(sboard[i - 1][j], sboard[i][j - 1]))
+		
+	return sboard[N][N]
 	
-	return ans_board[N][N]
-
 
 def main():
 	N = int(input())
 	board = [[0] * N for _ in range(N)]
+	
 	for i in range(N):
 		board[i] = list(map(int, input().split()))
 	
