a4c2e691ac91f96eada81b7e7f60508a99ca1d74
Sukhyun Nam <107014709+RGZ1890@users.noreply.github.com>
2025-01-12 00:40:00 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 305ms, Memory: 29MB, Status: Passed - CodeTree

---
+++ 250112/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -0,0 +1,49 @@
+dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+
+
+def moveable(board, n, cur, nex):
+	return board[nex[0]][nex[1]] != 0 and board[cur[0]][cur[1]] > board[nex[0]][nex[1]]
+
+
+def travel(aboard, board, n, cur):
+	if aboard[cur[0]][cur[1]] != 0:
+		return aboard[cur[0]][cur[1]]
+	for d in dirs:
+		nex = [cur[0] + d[0], cur[1] + d[1]]
+		if moveable(board, n, cur, nex):
+			aboard[cur[0]][cur[1]] = max(aboard[cur[0]][cur[1]], \
+				travel(aboard, board, n, nex) + 1)
+	return aboard[cur[0]][cur[1]]
+		
+
+def solution(board, n):
+	aboard = [[0] * (n + 2) for _ in range(n + 2)]
+	for i in range(1, n + 1):
+		for j in range(1, n + 1):
+			cur = [i, j]
+			cnt = 0
+			for d in dirs:
+				nex = [cur[0] + d[0], cur[1] + d[1]]
+				if moveable(board, n, cur, nex):
+					cnt += 1
+			aboard[cur[0]][cur[1]] = 1 if cnt == 0 else 0
+		
+	ans = 0
+	for i in range(1, n + 1):
+		for j in range(1, n + 1):
+			ans = max(ans, travel(aboard, board, n, [i, j]))
+			
+	return ans
+
+
+def main():
+	n = int(input())
+	board = [[0] * (n + 2) for _ in range(n + 2)]
+	for i in range(1, n + 1):
+		board[i] = [0] + list(map(int, input().split())) + [0]
+
+	print(solution(board, n))
+
+
+if __name__ == "__main__":
+	main()
